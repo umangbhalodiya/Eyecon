baddb6616ea77fa37f6b4aca956af37841de133e
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-21 23:06:42 +0530

    cmnt

--- src/Components/Routes/Header/header.js
+++ src/Components/Routes/Header/header.js
@@ -2,11 +2,9 @@ import "../../Styles/header.scss";
 
 const Header = () => {
   return (
-    <div>
       <div className="header-container">
         <div className="header-section">Eyecons</div>
       </div>
-    </div>
   );
 };
 
