542afc66558e4defe464acbdecd27899276f35d4
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-12-20 11:17:01 +0530

    old bootstrap component removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Routes/Header/header";
 import Footer from "./Components/Routes/Footer/footer";
 import Home from "./Components/Routes/Home/home";
 import Icons from "./Components/Routes/Icons/icons";
-import Bootstrapicons from "./Components/Routes/Icons/bootstrapicons";
-
 export default function App() {
-  const [iconsType, setIconsType] = useState("mui");
   return (
     <div className="container">
       <Header />
