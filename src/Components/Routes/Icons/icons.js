b90884e9b48cab7bf4c3fdf718997d3b83e2c413
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-12-21 23:28:22 +0530

    classic

--- src/Components/Routes/Icons/icons.js
+++ src/Components/Routes/Icons/icons.js
@@ -2,24 +2,27 @@ import { useState } from "react";
 import "../../Styles/icons.scss";
 import Bootstrap from "../Bootstrap/Bootstrap";
 import Doodle from "../Doodle/Doodle";
-import Normal from "../Normal/Normal";
+import Lucide from "../Lucide/Lucide";
+import Classic from "../Classic/Classic";
 
 const Icons = () => {
-  const [iconsType, setIconsType] = useState("normal");
+  const [iconsType, setIconsType] = useState("classic");
 
   return (
     <div className="icons-container">
       <div className="icons-items">
         <div className="icon-types">
-          <div className={iconsType === "normal" ? "icon-type-item-active" : "icon-type-item"} onClick={() => { setIconsType("normal") }}>Normal</div>
+          <div className={iconsType === "classic" ? "icon-type-item-active" : "icon-type-item"} onClick={() => { setIconsType("classic") }}>Classic</div>
           <div className={iconsType === "bootstrap" ? "icon-type-item-active" : "icon-type-item"} onClick={() => { setIconsType("bootstrap") }}>Bootstrap</div>
           <div className={iconsType === "doodle" ? "icon-type-item-active" : "icon-type-item"} onClick={() => { setIconsType("doodle") }}>Doodle</div>
+          <div className={iconsType === "lucide" ? "icon-type-item-active" : "icon-type-item"} onClick={() => { setIconsType("lucide") }}>Lucide</div>
         </div>
         <div className="result-icons">
           <div className="all-icons">
-            {iconsType === "normal" && <Normal />}
+            {iconsType === "classic" && <Classic />}
             {iconsType === "bootstrap" && <Bootstrap />}
             {iconsType === "doodle" && <Doodle />}
+            {iconsType === "Lucide" && <Lucide />}
           </div>
         </div>
       </div>
